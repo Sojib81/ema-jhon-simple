2fe057ab92dea0b91082ec273c6aea4f4ccce102
Sojib81 <sojibten4412@gmail.com>
2024-09-16 18:02:12 +0600

    button added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,32 @@
 import React from "react";
+import "./Product.css";
+import { FaShoppingCart } from "react-icons/fa";
 
 const product = (props) => {
   console.log(props.product.name);
+  const { name, url, price, img, seller, stock } = props.product;
+
   return (
-    <div>
-      <h2>{props.product.name}</h2>
+    <div className="product-section">
+      <div className="img-container">
+        <img src={img} alt="" />
+      </div>
+
+      <div className="product-details">
+        <h4>{name}</h4>
+        <p>
+          <small>by-{seller}</small>
+        </p>
+        <p>${price}</p>
+        <p>
+          <small>Only {stock} left in stock - order soon.</small>
+        </p>
+
+        <button className="btn-add-to-cart">
+          <FaShoppingCart />
+          Add to cart
+        </button>
+      </div>
     </div>
   );
 };
